af10fd0547600e9ca46e0ae107f4ecd9a62156a1
Vikki <vikkiroxx28@gmail.com>
2021-11-23 09:31:53 +0530

    .

---
+++ Pass_by_refference.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void pass_by_ref1(int &num);
+void pass_by_ref2(string &s);
+void pass_by_ref3(vector<string> &v);
+void print_vector(const vector<string> &v);
+
+int main()
+{
+    int num = 10;
+    int another_num = 20;
+
+    cout << "num before calling pass_by_ref1: " << num << endl;
+    pass_by_ref1(num);
+    cout << "num after calling pass_by_ref1: " << num << endl;
+
+    cout << "\nanother_num before calling pass_by_ref1: " << another_num << endl;
+    pass_by_ref1(another_num);
+    cout << "another_num after calling pass_by_ref1: " << another_num << endl;
+
+    string name = "Frank";
+    cout << "\nname before calling pass_by_ref2: " << name << endl;
+    pass_by_ref2(name);
+    cout << "name after calling pass_by_ref2: " << name << endl;
+
+    vector<string> stooges{"larry", "moe", "curly"};
+    cout << "\nstooges before calling pass_by_ref3: ";
+    print_vector(stooges);
+    pass_by_ref3(stooges);
+    cout << "stogges after calling pass_by_ref3: ";
+    print_vector(stooges);
+    return 0;
+}
+
+void pass_by_ref1(int &num)
+{
+    num = 1000;
+}
+void pass_by_ref2(string &s)
+{
+    s = "Changed";
+}
+void pass_by_ref3(vector<string> &v)
+{
+    v.clear();
+}
+void print_vector(const vector<string> &v)
+{
+    for (auto s : v)
+    {
+        cout << s << " ";
+    }
+    cout << endl;
+}
